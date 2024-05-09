d41e078c3e1b53fbf780bdc424db646d83e24a97
Daniel Holth <dholth@anaconda.com>
2024-05-09 16:50:53 -0400

    time hashing whole index.json, writing msgpack.zst

--- test_change_strategy.py
+++ test_change_strategy.py
@@ -7,9 +7,22 @@ import hashlib
 import itertools
 import json
 
+import msgpack
 import pytest
+import zstandard
 
 from changing_repodata import DateLimitedCache
+
+
+def pack_package_record(record):
+    """
+    Convert hex checksums to bytes.
+    """
+    if sha256 := record.get("sha256"):
+        record["sha256"] = bytes.fromhex(sha256)
+    if md5 := record.get("md5"):
+        record["md5"] = bytes.fromhex(md5)
+    return record
 
 
 def add_computed(db):
@@ -18,12 +31,15 @@ def add_computed(db):
 
     (re-computes on each query returning these columns).
     """
-    db.execute(
-        "ALTER TABLE index_json ADD COLUMN IF NOT EXISTS name AS (json_extract(index_json, '$.name'))"
-    )
-    db.execute(
-        "ALTER TABLE index_json ADD COLUMN IF NOT EXISTS sha256 AS (json_extract(index_json, '$.sha256')))"
-    )
+    columns = set(row[1] for row in db.execute("pragma table_xinfo(index_json)"))
+    if "name" not in columns:
+        db.execute(
+            "ALTER TABLE index_json ADD COLUMN IF NOT EXISTS name AS (json_extract(index_json, '$.name'))"
+        )
+    if "sha256" not in columns:
+        db.execute(
+            "ALTER TABLE index_json ADD COLUMN IF NOT EXISTS sha256 AS (json_extract(index_json, '$.sha256'))"
+        )
 
 
 def package_names_in_stat_table():
@@ -41,6 +57,8 @@ def test_shard_sums(index, benchmark):
     cache: DateLimitedCache = index.cache_for_subdir("linux-64")  # type: ignore
     # only files in the upstream stage get included in the final index
     upstream = cache.upstream_stage
+
+    add_computed(cache.db)
 
     # indexing's job is to make incoming sha256 and stat stage sha256 match by
     # extracting latest packages... we have also added a sha256 pulled from
@@ -71,6 +89,13 @@ def test_shard_sums(index, benchmark):
 
 @pytest.mark.parametrize("algorithm", [hashlib.sha256, hashlib.blake2b])
 def test_shard_sums_name_in_stat(index, benchmark, algorithm):
+    """
+    Test including a (populated by upsert) name in stat; see example in
+    package_names_in_stat_table function.
+
+    2.2x faster than test_shard_sums but only saves about 863ms on
+    conda-forge/linux-64.
+    """
     sums_by_name: dict[str, hashlib._Hash] = {}
     cache: DateLimitedCache = index.cache_for_subdir("linux-64")  # type: ignore
     # only files in the upstream stage get included in the final index
@@ -125,3 +150,85 @@ def test_generate_whole_repodata(index, subdir, benchmark):
         return json.dumps(repodata, separators=(":", ","), sort_keys=True)
 
     assert isinstance(generate, str)
+
+
+@pytest.mark.benchmark(min_rounds=2)
+def test_index_json_sums(index, benchmark):
+    """
+    Hash entire path, index_json contents to possibly find changes.
+    """
+    cache: DateLimitedCache = index.cache_for_subdir("linux-64")  # type: ignore
+    # only files in the upstream stage get included in the final index
+    upstream = cache.upstream_stage
+
+    # indexing's job is to make incoming sha256 and stat stage sha256 match by
+    # extracting latest packages... we have also added a sha256 pulled from
+    # index_json itself that we also added during the indexing phase.
+    @benchmark
+    def compute_sums():
+        sums = {}
+        for path, rows in itertools.groupby(
+            cache.db.execute(
+                """SELECT index_json.name, path, index_json
+                FROM stat JOIN index_json USING (path) WHERE stat.stage = ?
+                ORDER BY index_json.name, index_json.path""",
+                (upstream,),
+            ),
+            lambda k: k[0],
+        ):
+            hasher = hashlib.sha256()
+            for row in rows:
+                name, path, index_json = row
+                hasher.update((path + index_json).encode())
+            sums[name] = hasher.hexdigest()
+
+        return sums
+
+    assert compute_sums is not None
+
+
+@pytest.mark.benchmark(min_rounds=2)
+def test_compress_shards(index, tmp_path, benchmark):
+    """
+    Generate and write-to-disk unpatched repodata shards.
+    """
+    cache: DateLimitedCache = index.cache_for_subdir("linux-64")  # type: ignore
+    # only files in the upstream stage get included in the final index
+    upstream = cache.upstream_stage
+    compressor = zstandard.ZstdCompressor()
+
+    @benchmark
+    def compress_shards():
+        shards = {}
+        for name, rows in itertools.groupby(
+            cache.db.execute(
+                """SELECT index_json.name, path, index_json
+                FROM stat JOIN index_json USING (path) WHERE stat.stage = ?
+                ORDER BY index_json.name, index_json.path""",
+                (upstream,),
+            ),
+            lambda k: k[0],
+        ):
+            shard = {"packages": {}, "packages.conda": {}}
+            hasher = hashlib.sha256()
+            for row in rows:
+                name, path, index_json = row
+                assert path.endswith(
+                    (".tar.bz2", ".conda")
+                ), f"Unknown package filename {path}"
+                hasher.update((path + index_json).encode())
+                record = json.loads(index_json)
+                key = "packages" if path.endswith(".tar.bz2") else "packages.conda"
+                shard[key][path] = pack_package_record(record)
+
+            reference_hash = hasher.hexdigest()
+
+            (tmp_path / f"{name}-{reference_hash}.msgpack.zst").write_bytes(
+                compressor.compress(msgpack.packb(shard))  # type: ignore
+            )
+
+            shards[name] = shard
+
+        return shards
+
+    assert compress_shards is not None
